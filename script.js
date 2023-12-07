0656fb2f958e7be16520014dcb454aff9bb7d92d
TIMISONG-dev <dima.pilaer@gmail.com>
2023-12-07 19:19:34 +0300

    add: animations for box & scroll

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const row1 = document.getElementById('row1');
+    const row2 = document.getElementById('row2');
+  
+    document.addEventListener('scroll', function() {
+      const scrollPosition = window.scrollY;
+  
+      if (scrollPosition > 100) {
+        row1.style.opacity = 0;
+        row2.style.opacity = 1;
+      } else {
+        row1.style.opacity = 1;
+        row2.style.opacity = 0;
+      }
+    });
+  });
