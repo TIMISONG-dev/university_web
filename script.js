4c180464156802960be11b4e70ed07492728ef4c
TIMISONG-dev <dima.pilaer@gmail.com>
2023-12-08 19:32:59 +0300

    update: blured navbar, new icon, etc

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const row1 = document.getElementById('row1');
-    const row2 = document.getElementById('row2');
-  
-    document.addEventListener('scroll', function() {
-      const scrollPosition = window.scrollY;
-  
-      if (scrollPosition > 200) {
-        row1.style.opacity = 0;
-        row2.style.opacity = 1;
-      } else {
-        row1.style.opacity = 1;
-        row2.style.opacity = 0;
-      }
-    });
+  const row1 = document.getElementById('row1');
+  const row2 = document.getElementById('row2');
+
+  document.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 200) {
+      row1.style.opacity = 0;
+      row2.style.opacity = 1;
+    } else {
+      row1.style.opacity = 1;
+      row2.style.opacity = 0;
+    }
   });
+});
